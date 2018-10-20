62c8353b6854792fc112452327ec38d524b08429
Yesenia <eliyessie@gmail.com>
2018-10-19 21:07:09 -0700

    added routes, router-oulet not recognized

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,26 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { FooterComponent } from './footer/footer.component';
+import { AppRoutingModule } from './modules/app-routing/app-routing.module';
+import { HomeComponent } from './components/home/home.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+
+import { RouterModule, Router } from '@angular/router';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    ContactComponent,
+    ProjectsComponent,
+    RouterModule
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes, Router } from '@angular/router';
+import { HomeComponent } from 'src/app/components/home/home.component';
+import { ContactComponent } from 'src/app/components/contact/contact.component';
+import { ProjectsComponent } from 'src/app/components/projects/projects.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'projects', component: ProjectsComponent }
+];
+
+@NgModule({
+  imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  declarations: []
+})
+export class AppRoutingModule { }
